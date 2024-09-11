2ba09b85e5f486818a5ead6e29a8b876e38489a9
Nicolas Robles <nicolasrobles98@gmail.com>
2024-09-11 15:34:51 -0300

    Mi primera clase

---
+++ src/Domain/Entities/Usuario.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public abstract class Usuario
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }  = string.Empty;
+        public string Apellido { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string Contrasenia { get; set; } = string.Empty;
+        public string NombreUser { get; set; } = string.Empty;
+
+    }
+}
