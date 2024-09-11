46a9f659f002e2bdfc96268acb7fdbbe1eb1cccb
Nicolas Robles <nicolasrobles98@gmail.com>
2024-09-11 15:43:10 -0300

    Mis otras first clases

---
+++ src/Domain/Entities/Dueño.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public class Dueño : Usuario
+    {
+        public ICollection<Mensaje> Mensajes { get; set; } = new List<Mensaje>();
+        public ICollection<Taller> Talleres { get; set; } = new List<Taller>();
+    }
+}
